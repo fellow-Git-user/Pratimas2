a79bcdb64e51fba09310170931be3faac7ce905f
fellow-Git-user <martynas.nadeika@gmail.com>
2024-12-08 15:08:29 +0200

    second exercie done

---
+++ JS/script.js
@@ -0,0 +1,76 @@
+let a = 10;
+let b = 57;
+
+console.log((`answer is ${a + b}`));
+
+
+let year = 1999 - 25;
+console.log(year);
+
+// CodeAcademy pvz
+// let year = 1999;
+// year -= 25;
+// console.log(year);
+
+let myName = "Martynas";
+let mySurname = "Nadeika";
+console.log(`${myName + " " + mySurname}`);
+
+// CodeAcademy pvz
+// const name = "Gedas";
+// const lastname = "Gardauskas";
+// const fullname = name + " " + lastname;
+// console.log(fullname);
+
+
+
+// const userInput = prompt("Please write in temperature in Celsius to be converted to Farenheit", "Write it now");
+// if (userInput !== null && !isNaN(+userInput)){
+//     const celsius = +userInput;
+//     const farenheit = celsius * 1.8 + 32;
+//     console.log(`You wrote in ${celsius} so conversion formula is ${celsius} * 1.8 + 32`);
+//     console.log("Answer is " + farenheit);
+// }
+// else {
+//     alert("You canceled the input.");
+// }
+
+
+// let tempInCelsius = x;
+// let tempInFarenheit = x * 1.8 + 32;
+
+
+
+// let initialBalance = 999.99;
+// let newBalance = 999.99 + (initialBalance * 9 / 100);
+// console.log(`Balance is ${newBalance}`);
+// newBalance = newBalance * 2;
+// console.log(`Balance is ${newBalance}`);
+// newBalance = newBalance - 50;
+// console.log(`Balance is ${newBalance}`);
+// newBalance = newBalance.toFixed(2);
+// console.log(`Balance is ${newBalance}`);
+
+// console.log(newBalance);
+
+// let name = "Martynas";
+// let surname = "Vanagas";
+
+// console.log(name === surname);
+
+
+// let number = 10;
+// let text = "10";
+
+// let resultFirst = number > text;
+// let resultSecond = number == text;
+// let resultThird = number === text;
+// console.log(`${resultFirst}, ${resultSecond}, ${resultThird}`)
+
+// let number1 = 2;
+// let number2 = 3;
+
+// let result = number1 % number2 === 0;
+// console.log(result);
+
+
